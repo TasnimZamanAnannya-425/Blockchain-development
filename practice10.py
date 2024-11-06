c87505363a671db60c6cbe510c317b1d1f23b36d
Tasnim Zaman <100079990+TasnimZamanAnannya-425@users.noreply.github.com>
2024-11-06 18:02:08 +0600

    Add files via upload

---
+++ practice10.py
@@ -0,0 +1,8 @@
+user_input1= input("input a value of A : ")
+user_input2= input("input a value of B : ")
+user_input3= input("input a value of C : ")
+
+
+print("vaue of a",user_input1)
+print("vaue of b",user_input2)
+print("vaue of c",user_input3)
